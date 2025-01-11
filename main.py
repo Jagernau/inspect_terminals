a6dba0864c7170800485f0d99eac8c2b7ce331af
jagernau <stepanenkov25@gmail.com>
2025-01-11 23:08:24 +0300

    exception cicle

--- main.py
+++ main.py
@@ -44,53 +44,53 @@ async def process_glonass_system(config):
         chunked_vehicles = chunk_list(new_vehicles_len, 100)
         for chunk in chunked_vehicles:
             try:
-            # Рассылка команд
-            await glonass_action.put_comands(chunk, sorted_device_type_counts)
-            await asyncio.sleep(10)
+                # Рассылка команд
+                await glonass_action.put_comands(chunk, sorted_device_type_counts)
+                await asyncio.sleep(10)
 
-            # Сбор ответов
-            answers = await glonass_action.answer_objects(chunk, sorted_device_type_counts)
-            if len(answers) >= 1:
-                for answer in answers:
-                    if answer["monitoring_system"] and answer["vehicleId"]:
-                        client_info = crud.get_client_info_from_obj({
-                            "monitoring_system": answer.get("monitoring_system"),
-                            "vehicleId": answer.get("vehicleId"),
-                        })
-                        availability_sim_in_db = crud.get_availability_sim_in_db(
-                                answer.get("iccid")
-                                )
-                        if_change_imei = 0
-                        old_sim_imei = None
-                        if availability_sim_in_db == 1:
-                            sim_db_imei = crud.get_sim_imei(answer.get("iccid"))
-                            old_sim_imei = sim_db_imei
-                            crud.update_sim_client(
-                                    sim_iccid=answer.get("iccid"),
-                                    sim_client_id=int(client_info["client_id"])
+                # Сбор ответов
+                answers = await glonass_action.answer_objects(chunk, sorted_device_type_counts)
+                if len(answers) >= 1:
+                    for answer in answers:
+                        if answer["monitoring_system"] and answer["vehicleId"]:
+                            client_info = crud.get_client_info_from_obj({
+                                "monitoring_system": answer.get("monitoring_system"),
+                                "vehicleId": answer.get("vehicleId"),
+                            })
+                            availability_sim_in_db = crud.get_availability_sim_in_db(
+                                    answer.get("iccid")
                                     )
-                            if sim_db_imei != answer.get("imei"):
-                                if_change_imei = 1
-                                crud.update_sim_imei(
-                                        answer_iccid=str(answer["iccid"]),
-                                        answer_imei=str(answer["imei"])
+                            if_change_imei = 0
+                            old_sim_imei = None
+                            if availability_sim_in_db == 1:
+                                sim_db_imei = crud.get_sim_imei(answer.get("iccid"))
+                                old_sim_imei = sim_db_imei
+                                crud.update_sim_client(
+                                        sim_iccid=answer.get("iccid"),
+                                        sim_client_id=int(client_info["client_id"])
                                         )
-                        
-                        marge_full_info = {
-                            "type_term": answer["type"],
-                            "imei": answer["imei"],
-                            "iccid": answer["iccid"],
-                            "monitoring_system": answer["monitoring_system"],
-                            "vehicleId": answer.get("vehicleId"),
-                            "vehicle_name": answer.get("vehicle_name"),
-                            "client_name": client_info.get("client_name"),
-                            "client_id": client_info.get("client_id"),
-                            "iccid_in_db": availability_sim_in_db,
-                            "if_change_imei": if_change_imei,# Изменяется ли imei
-                            "old_sim_imei": old_sim_imei,# imei SIM до изменений
-                        }
-                        crud.add_inspect_terminal(marge_full_info)
-                        log.info(marge_full_info)
+                                if sim_db_imei != answer.get("imei"):
+                                    if_change_imei = 1
+                                    crud.update_sim_imei(
+                                            answer_iccid=str(answer["iccid"]),
+                                            answer_imei=str(answer["imei"])
+                                            )
+                            
+                            marge_full_info = {
+                                "type_term": answer["type"],
+                                "imei": answer["imei"],
+                                "iccid": answer["iccid"],
+                                "monitoring_system": answer["monitoring_system"],
+                                "vehicleId": answer.get("vehicleId"),
+                                "vehicle_name": answer.get("vehicle_name"),
+                                "client_name": client_info.get("client_name"),
+                                "client_id": client_info.get("client_id"),
+                                "iccid_in_db": availability_sim_in_db,
+                                "if_change_imei": if_change_imei,# Изменяется ли imei
+                                "old_sim_imei": old_sim_imei,# imei SIM до изменений
+                            }
+                            crud.add_inspect_terminal(marge_full_info)
+                            log.info(marge_full_info)
             except Exception as e:
                 log.error(f"Ошибка при работе с ГЛОНАСС: {e}")
                 continue
